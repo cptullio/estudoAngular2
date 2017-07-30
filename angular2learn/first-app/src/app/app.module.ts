ec926634aaa31c7991d20aec6a030283bf5eb740
Carlos <cptullio@gmail.com>
2017-07-30 11:56:01 -0300

    primeiro componente

--- angular2learn/first-app/src/app/app.module.ts
+++ angular2learn/first-app/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MeuPrimeiroComponente } from './primeiro-componente/meu-primeiro-componente.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponente
   ],
   imports: [
     BrowserModule
---
+++ angular2learn/first-app/src/app/primeiro-componente/meu-primeiro-componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meuprimeiroComponent',
+  templateUrl: './meu.primeiro.componente.html'
+})
+export class MeuPrimeiroComponente {
+  title = 'carlos';
+}
